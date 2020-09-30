74d6693f958f166c5c2dd16b08b56e7ae9ad94d3
dar4kamal <sasaa9646@gmail.com>
2020-09-30 12:52:38 +0200

    added Landing PAge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Landing from "./components/Landing";
+import Login from "./components/Login";
+
+const App = () => {
+	return (
+		<Router>
+			<Fragment>
+				<Container>
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/login" component={Login} />
+					</Switch>
+				</Container>
+			</Fragment>
+		</Router>
+	);
+};
 
 export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+	return (
+		<Fragment>
+			<div>
+				<img
+					variant="top"
+					src="https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?size=338&ext=jpg"
+					alt="home"
+					width="90%"
+					heigth="90%"
+					className="rounded mx-auto d-block mb-4"
+				/>
+			</div>
+			<div className="bg-light">
+				<Card
+					className="text-center mt-4"
+					style={{ width: "100%", border: "none" }}
+				>
+					<Card.Body>
+						<Card.Title>Welcome to our app!</Card.Title>
+						<Card.Text>
+							This is the first version of user profile app.
+						</Card.Text>
+						<Card.Text>Please sign in or create an account below</Card.Text>
+						<Link
+							to="/login"
+							style={{ width: "100%" }}
+							variant="primary"
+							className="btn btn-primary m-2"
+						>
+							Log in
+						</Link>
+						<Link
+							to="/register"
+							style={{ width: "100%" }}
+							variant="primary"
+							className="m-2 btn btn-primary"
+						>
+							Create New Account
+						</Link>
+					</Card.Body>
+				</Card>
+			</div>
+		</Fragment>
+	);
+};
+
+export default Landing;
