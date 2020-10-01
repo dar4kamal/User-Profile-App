e550f5299957b5d8cb67bc88c7751813df46877c
dar4kamal <sasaa9646@gmail.com>
2020-10-01 04:32:49 +0200

    updated ChangePass UI

--- src/components/editForms/ChangePass.js
+++ src/components/editForms/ChangePass.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
 import Alert from "../Alert";
@@ -51,33 +51,61 @@ const ChangePass = () => {
 	if (loaded) return <Redirect to="/dashboard" />;
 
 	return (
-		<Fragment>
-			<h1 className="large text-primary">Change Password</h1>
-			<form className="form" onSubmit={(e) => onSubmit(e)}>
-				<div className="form-group">
-					<label> Password</label>
+		<div
+			style={{
+				backgroundColor: "#53D7BC",
+				borderTopRightRadius: "25px",
+				borderTopLeftRadius: "25px",
+			}}
+		>
+			<div className="pt-5">
+				<h1
+					className="large pt-5 pl-4 pb-4"
+					style={{
+						color: "white",
+						fontSize: 26,
+					}}
+				>
+					Change Password
+				</h1>
+			</div>
+			<div
+				className="bg-light"
+				style={{ borderTopRightRadius: "25px", borderTopLeftRadius: "25px" }}
+			>
+				<form className="form" onSubmit={(e) => onSubmit(e)}>
+					<div className="form-group pt-4 pl-4 pr-4">
+						<label>New Password</label>
+						<input
+							type="password"
+							className="form-control"
+							placeholder="Password"
+							name="newPassword"
+							value={newPassword}
+							required
+							onChange={(e) => onChange(e)}
+						/>
+					</div>
+					<div className="form-group pt-3 pl-4 pr-4">
+						<label> Re-type Password</label>
+						<input
+							type="password"
+							className="form-control"
+							placeholder="Confirm Password"
+							name="passwordConfirm"
+							required
+							value={passwordConfirm}
+							onChange={(e) => onChange(e)}
+						/>
+					</div>
 					<input
-						type="password"
-						placeholder="Password"
-						name="newPassword"
-						value={newPassword}
-						required
-						onChange={(e) => onChange(e)}
+						type="submit"
+						className="btn btn-primary mt-3 mr-2 ml-3 float-none"
+						style={{ width: "90%" }}
+						value="Change Password"
 					/>
-				</div>
-				<div className="form-group">
-					<label> Re-type Password</label>
-					<input
-						type="password"
-						placeholder="Confirm Password"
-						name="passwordConfirm"
-						required
-						value={passwordConfirm}
-						onChange={(e) => onChange(e)}
-					/>
-				</div>
-				<input type="submit" className="btn btn-primary" value="Register" />
-			</form>
+				</form>
+			</div>
 			{errors ? (
 				errors.map((e) => {
 					return <Alert key={e.msg} msg={e.msg} />;
@@ -85,7 +113,7 @@ const ChangePass = () => {
 			) : (
 				<div></div>
 			)}
-		</Fragment>
+		</div>
 	);
 };
 
