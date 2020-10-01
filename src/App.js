d73512f56e4ca32ec13ed4d4a04d23d6331395e7
dar4kamal <sasaa9646@gmail.com>
2020-10-01 02:26:36 +0200

    removed App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import "./App.css";
 
 import Landing from "./components/Landing";
 import Login from "./components/Login";
@@ -15,17 +13,15 @@ const App = () => {
 	return (
 		<Router>
 			<Fragment>
-				<Container>
-					<Switch>
-						<Route exact path="/" component={Landing} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/register" component={Register} />
-						<Route exact path="/dashboard" component={Dashboard} />
-						<Route exact path="/forget" component={ForgetPassword} />
-						<Route exact path="/reset" component={ResetPassword} />
-						<Route exact path="/edit-password" component={ChangePass} />
-					</Switch>
-				</Container>
+				<Switch>
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/dashboard" component={Dashboard} />
+					<Route exact path="/forget" component={ForgetPassword} />
+					<Route exact path="/reset" component={ResetPassword} />
+					<Route exact path="/edit-password" component={ChangePass} />
+				</Switch>
 			</Fragment>
 		</Router>
 	);
