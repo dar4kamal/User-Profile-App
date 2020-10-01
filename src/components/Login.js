db87c35f8da046fac9d0f6772386bc5e46ced7f9
dar4kamal <sasaa9646@gmail.com>
2020-10-01 03:16:02 +0200

    updated Login UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import Alert from "./Alert";
 
@@ -41,38 +41,71 @@ const Login = () => {
 
 	if (loaded) return <Redirect to="/dashboard" />;
 	return (
-		<Fragment>
-			<Link to="/">
-				<i className="fas fa-arrow-left"></i>
-			</Link>
+		<div
+			style={{
+				backgroundColor: "#53D7BC",
+			}}
+		>
+			<div className="w-100">
+				<div className="pt-5 pb-2 pl-4 mb-2">
+					<Link to="/">
+						<i
+							className="fas fa-m fa-arrow-left"
+							style={{ color: "white" }}
+						></i>
+					</Link>
+				</div>
 
-			<h4 className="large text-primary">log in to your account</h4>
-			<form className="form" onSubmit={(e) => onSubmit(e)}>
-				<div className="form-group">
+				<h4
+					className="large pl-4 pb-4"
+					style={{
+						color: "white",
+						fontSize: 26,
+					}}
+				>
+					log in to your account
+				</h4>
+			</div>
+			<div
+				className="bg-light"
+				style={{ borderTopRightRadius: "25px", borderTopLeftRadius: "25px" }}
+			>
+				<form onSubmit={(e) => onSubmit(e)}>
+					<div className="form-group pt-5 pl-4 pr-4">
+						<label>Email</label>
+						<input
+							className="form-control"
+							type="email"
+							placeholder="Please enter your email"
+							name="email"
+							value={email}
+							onChange={(e) => onChange(e)}
+							required
+						/>
+					</div>
+					<div className="form-group pl-4 pr-4">
+						<label>Password</label>
+						<input
+							className="form-control"
+							type="password"
+							placeholder="Please enter your password"
+							name="password"
+							value={password}
+							onChange={(e) => onChange(e)}
+						/>
+					</div>
+					<Link to="/forget" className="float-right pr-4">
+						<small>Forget Password?</small>
+					</Link>
+					<br />
 					<input
-						type="email"
-						placeholder="Please enter your email"
-						name="email"
-						value={email}
-						onChange={(e) => onChange(e)}
-						required
+						type="submit"
+						className="btn btn-primary mt-3 mr-2 ml-3 float-none"
+						style={{ width: "90%" }}
+						value="Login"
 					/>
-				</div>
-				<div className="form-group">
-					<input
-						type="password"
-						placeholder="Please enter your password"
-						name="password"
-						value={password}
-						onChange={(e) => onChange(e)}
-					/>
-				</div>
-				<Link to="/forget" className="float-right">
-					Forget Password?
-				</Link>
-				<br />
-				<input type="submit" className="btn btn-primary" value="Login" />
-			</form>
+				</form>
+			</div>
 			{errors ? (
 				errors.map((e) => {
 					return <Alert key={e.msg} msg={e.msg} />;
@@ -80,7 +113,7 @@ const Login = () => {
 			) : (
 				<div></div>
 			)}
-		</Fragment>
+		</div>
 	);
 };
 
